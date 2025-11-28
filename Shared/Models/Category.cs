a1620cece67453cb05a468af1605bcf7975e3836
ToryLyt <Litvi_vf12@student.itstep.org>
2025-11-28 20:49:11 +0100

    add Category Project SubCategory edit Transqaction

---
+++ Shared/Models/Category.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Models
+{
+    public class Category
+    {
+        private int _id;
+        private string _name="";
+        private string _description = "";
+
+        public int Id
+        { 
+            get { return _id; }
+            set {  _id = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+    }
+}
---
+++ Shared/Models/Project.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Shared.Models
+{
+    public class Project
+    {
+        private int _id;
+        private string _name="";
+        private string _description="";
+        private bool _isActive=true;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+        public bool IsActive
+        {
+            get { return _isActive; }
+            set { _isActive = value; }
+        }
+    }
+}
